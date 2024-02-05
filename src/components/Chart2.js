78337f987686ad4e39a69c09c8178ec8ea17676f
Patryk <patrykkonior4@gmail.com>
2024-02-05 16:15:08 +0100

    Add highcharts

--- src/components/Chart2.js
+++ src/components/Chart2.js
@@ -1,5 +1,19 @@
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const options = {
+  series: [
+    {
+      name: "Bilans energii",
+      kWh: ["0tys.", "50tys.", "100tys.", "150tys."],
+    },
+  ],
+};
+
 export default function Chart2() {
-    return <div>
-        <p>CHART FROM HIGHCHARTS.JS</p>
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
+  );
 }
