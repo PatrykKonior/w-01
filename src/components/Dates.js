6dcdf8a20f110de36fd04d5e49c37543889734d9
Patryk <patrykkonior4@gmail.com>
2024-02-17 21:46:41 +0100

    Update

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -1,18 +1,20 @@
 export default function Dates() {
   return (
-    <div>
-      <p>Gospodarka wodociągowa</p>
-      <input
-        type="date"
-        id="start"
-        name="search-date"
-        value="2024-03-01"
-        min="2000-01-01"
-        max="2100-01-01"
-      />
-      <button>Data</button>
-      <button>Miesiąc</button>
-      <button>Rok</button>
+    <div className="w-dvw bg-slate-500 rounded-2xl overflow-auto relative flex p-5 m-4 inset-y-1 right-5">
+      <div className="flex space-x-4 object-right">
+        <p className="text-1xl text-white font-bold">Gospodarka wodociągowa</p>
+        <input
+          type="date"
+          id="start"
+          name="search-date"
+          value="2024-03-01"
+          min="2000-01-01"
+          max="2100-01-01"
+        />
+        <button>Data</button>
+        <button>Miesiąc</button>
+        <button>Rok</button>
+      </div>
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,19 @@
 export default function Menu() {
   return (
-    <div>
-      <img src="" alt="patryk konior" />
-      <div>
-        <img src="" alt="gospodarka sciekowa" />
-        <p>Gospodarka ściekowa</p>
-        <img src="" alt="gospodarka sciekowa" />
-        <p>Gospodarka wodna</p>
-        <img src="" alt="gospodarka sciekowa" />
-        <p>Analiza porównawcza</p>
-        <img src="" alt="gospodarka sciekowa" />
-        <p>Analiza czasowa</p>
-        <img src="" alt="gospodarka sciekowa" />
-        <p>Wskaźniki</p>
+    <div className="bg-emerald-950 w-72 h-auto flex flex-col">
+      <div className="text-white">
+        <img src="" alt="patryk konior" />
+        <div>
+          <p>Gospodarka ściekowa</p>
+          <img src="" alt="gospodarka sciekowa" />
+          <p>Gospodarka wodna</p>
+          <img src="" alt="gospodarka sciekowa" />
+          <p>Analiza porównawcza</p>
+          <img src="" alt="gospodarka sciekowa" />
+          <p>Analiza czasowa</p>
+          <img src="" alt="gospodarka sciekowa" />
+          <p>Wskaźniki</p>
+        </div>
       </div>
     </div>
   );
