eea3825b16471aa5ebdb9a1d29b3c99a3b994ac4
Patryk <patrykkonior4@gmail.com>
2024-02-17 15:21:37 +0100

    Start Dates and Menu components

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -1,5 +1,18 @@
 export default function Dates() {
-    return <div>
-        <p>DATES</p>
+  return (
+    <div>
+      <p>Gospodarka wodociągowa</p>
+      <input
+        type="date"
+        id="start"
+        name="search-date"
+        value="2024-03-01"
+        min="2000-01-01"
+        max="2100-01-01"
+      />
+      <button>Data</button>
+      <button>Miesiąc</button>
+      <button>Rok</button>
     </div>
+  );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,19 @@
 export default function Menu() {
-    return <div>
-        <p>VERTICAL MENU</p>
+  return (
+    <div>
+      <img src="" alt="patryk konior" />
+      <div>
+        <img src="" alt="gospodarka sciekowa" />
+        <p>Gospodarka ściekowa</p>
+        <img src="" alt="gospodarka sciekowa" />
+        <p>Gospodarka wodna</p>
+        <img src="" alt="gospodarka sciekowa" />
+        <p>Analiza porównawcza</p>
+        <img src="" alt="gospodarka sciekowa" />
+        <p>Analiza czasowa</p>
+        <img src="" alt="gospodarka sciekowa" />
+        <p>Wskaźniki</p>
+      </div>
     </div>
+  );
 }
