faccbe74a4e46e244cea8e99a021af0dafb7d51a
Patryk <patrykkonior4@gmail.com>
2024-02-18 19:11:13 +0100

    Update Header and Dates

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -1,8 +1,10 @@
 export default function Dates() {
   return (
-    <div className="col-span-4 col-start-2 row-start-2 bg-slate-500">
-      <div className="">
-        <p className="text-1xl text-white font-bold">Gospodarka wodociągowa</p>
+    <div className="col-span-4 col-start-2 row-start-2 bg-slate-500 rounded-2xl overflow-auto relative flex p-5 m-1 -inset-y-80 right-2 h-14">
+      <p className="text-1xl text-white font-bold absolute align-bottom top-4 ">
+        Gospodarka wodociągowa
+      </p>
+      <div className="flex space-x-6 object-right absolute align-bottom top-3 right-5 rounded-md">
         <input
           type="date"
           id="start"
@@ -10,10 +12,17 @@ export default function Dates() {
           value="2024-03-01"
           min="2000-01-01"
           max="2100-01-01"
+          className="text-1xl h-8 w-32 rounded-r-lg focus:outline-none bg-teal-600 text-white"
         />
-        <button>Data</button>
-        <button>Miesiąc</button>
-        <button>Rok</button>
+        <button className="text-1xl h-8 w-14 rounded-r-lg focus:outline-none bg-teal-800 text-white">
+          Data
+        </button>
+        <button className="text-1xl h-8 w-20 rounded-b-lg focus:outline-none bg-teal-800 text-white">
+          Miesiąc
+        </button>
+        <button className="text-1xl h-8 w-14 rounded-l-lg focus:outline-none bg-teal-800 text-white">
+          Rok
+        </button>
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 export default function Header() {
   return (
-    <div className="col-span-4 bg-emerald-950">
-      <p className="text-1xl text-white">
+    <div className="col-span-4 bg-emerald-950 relative h-14 rounded-1xl">
+      <p className="text-1xl text-white font-bold absolute inset-y-3 right-11">
         <span className="text-neutral-300 font-light">
           Zalogowany użytkownik:
         </span>
         Operator
       </p>
-      <button className="">&#9776;</button>
+      <button className="text-1xl absolute align-bottom top-1 right-0 h-7 w-7 m-1 focus:outline-none bg-white">
+        &#9776;
+      </button>
     </div>
   );
 }
