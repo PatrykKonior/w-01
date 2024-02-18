9e2a74d2852a2cfb48627840ea302e42c90e778f
Patryk <patrykkonior4@gmail.com>
2024-02-18 19:42:30 +0100

    Start Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,30 @@
 export default function Menu() {
   return (
-    <div className="row-span-6 bg-emerald-950">
+    <div className="row-span-6 bg-emerald-950 flex flex-col">
       <div className="text-white">
-        <img src="" alt="patryk konior" />
+        <img src="src\img\dragon-boat-festival (1).png" alt="patryk konior" />
         <div>
+          <img src="src\img\dragon-boat-festival (1).png" alt="dragon" />
           <p>Gospodarka ściekowa</p>
-          <img src="" alt="gospodarka sciekowa" />
+          <img
+            src="src\img\dragon-boat-festival (1).png"
+            alt="gospodarka sciekowa"
+          />
           <p>Gospodarka wodna</p>
-          <img src="" alt="gospodarka sciekowa" />
+          <img
+            src="src\img\dragon-boat-festival (1).png"
+            alt="gospodarka sciekowa"
+          />
           <p>Analiza porównawcza</p>
-          <img src="" alt="gospodarka sciekowa" />
+          <img
+            src="src\img\dragon-boat-festival (1).png"
+            alt="gospodarka sciekowa"
+          />
           <p>Analiza czasowa</p>
-          <img src="" alt="gospodarka sciekowa" />
+          <img
+            src="src\img\dragon-boat-festival (1).png"
+            alt="gospodarka sciekowa"
+          />
           <p>Wskaźniki</p>
         </div>
       </div>
