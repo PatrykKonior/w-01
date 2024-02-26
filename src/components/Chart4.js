6ffadfb918a304b97e1b0127e81ea4e514375d53
Patryk <patrykkonior4@gmail.com>
2024-02-26 20:32:23 +0100

    Start another Charts

--- src/components/Chart4.js
+++ src/components/Chart4.js
@@ -2,10 +2,63 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 const options = {
+  chart: {
+    type: "areaspline",
+  },
+  plotOptions: {
+    areaspline: {
+      color: {
+        linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+        stops: [
+          [0, "rgb(116,140,127)"], // Start color
+          [1, "rgba(233,245,234,1)"], // End color
+        ],
+      },
+    },
+  },
+  title: {
+    text: "Ew/Ez [%]",
+    align: "left",
+  },
+  xAxis: {
+    categories: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+  },
+  yAxis: {
+    title: {
+      text: "", // Hide the text "Values" on the yAxis
+    },
+    labels: {
+      enabled: false, // Hide the labels on the yAxis
+    },
+    plotLines: [
+      {
+        color: "red",
+        width: 2,
+        value: 100,
+        zIndex: 5, // Ensure the plot line is above the series
+        label: {
+          text: "100 %",
+          align: "right",
+          style: {
+            color: "red",
+            fontWeight: "bold",
+          },
+        },
+      },
+    ],
+  },
   series: [
     {
-      name: "Bilans energii",
-      kWh: ["0tys.", "50tys.", "100tys.", "150tys."],
+      name: "Ew/Ez [%]",
+      data: [80, 90, 96, 98, 99, 98, 97, 97, 96.7, 102, 98, 85],
+      color: "rgb(71,92,80)",
+      fillColor: {
+        linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+        stops: [
+          [0, "rgb(116,140,127)"], // Start color
+          [1, "rgba(233,245,234,1)"], // End color
+        ],
+      },
     },
   ],
 };
