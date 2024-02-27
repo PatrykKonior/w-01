9502edb2071784098e6efb2b948551855bc4f90c
Patryk <patrykkonior4@gmail.com>
2024-02-27 22:33:06 +0100

    Start changes in chart3

--- src/components/Chart3.js
+++ src/components/Chart3.js
@@ -17,11 +17,10 @@ const options = {
     },
   },
   title: {
-    text: "Ew/Ez [%]",
-    align: "left",
+    text: "Bilans energii",
   },
   xAxis: {
-    categories: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+    categories: ["Energia wyprodukowana", "Energia zużyta"],
   },
   yAxis: {
     title: {
